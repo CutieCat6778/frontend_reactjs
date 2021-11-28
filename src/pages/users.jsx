7cbdca8dceab8cfac54410de8907ccad60a41117
CutieCat6778 <thinhnguyenhuuhung@gmail.com>
2021-11-28 17:44:50 +0100

    semi push

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -5,25 +5,25 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export function Users() {
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
 
-  const id = useParams().userid;
+  // const id = useParams().userid;
 
-  useEffect(() => {
-    async function searchUser(id) {
-      const { data } = await axios.get('http://localhost/user/id/' + id);
-      setUser(data);
-    }
-    searchUser(id);
-  }, [id]);
+  // useEffect(() => {
+  //   async function searchUser(id) {
+  //     const { data } = await axios.get('http://localhost/user/id/' + id);
+  //     setUser(data);
+  //   }
+  //   searchUser(id);
+  // }, [id]);
 
-  if (!user) return null;
+  // if (!user) return null;
 
   return (
     <Box>
-      <Heading color="red.400">Xin chào {user.data.details.username}</Heading>
+      <Heading color="red.400">Xin chào</Heading>
       <Text>
-        Level hiện tại của bạn là <Code>{user.data.level}</Code> và lượng kinh nghiệm hiện tại của bạn là <Code>{user.data.exp}</Code>. Tổng bạn đã nhắn <Code>{user.data.total}</Code>
+        Test
       </Text>
     </Box>
   );
